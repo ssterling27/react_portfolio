94d9cf18553da914fdf7262655006622fb48e73b
ssterling27 <samuel.steinberg27@gmail.com>
2021-11-10 11:57:28 -0800

    establishing app

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # react_portfolio
+
+This is my personal portfolio, including skills and projects, created with React.
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
---
+++ server.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+
+const express = require('express')
+const { join } = require('path')
+
+const app = express()
+app.use(express.static(join(__dirname, 'client', 'build')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+if (process.env.NODE_ENV === 'production') {
+  app.get('*', (req, res) => {
+    res.sendFile(join(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
+syncDB()
+  .then(() => app.listen(process.env.PORT || 3001))
+  .catch(err => console.log(err))
